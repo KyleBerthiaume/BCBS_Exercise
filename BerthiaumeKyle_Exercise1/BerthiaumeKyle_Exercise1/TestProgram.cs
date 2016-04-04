d24d9388ca05abf2136b25720634c45af484157d
Kyle Berthiaume <berthiauky@mnstate.edu>
2016-04-04 14:42:54 -0500

    Exercise1 Submission

---
+++ BerthiaumeKyle_Exercise1/BerthiaumeKyle_Exercise1/TestProgram.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Berthiaume_Multiple; // Multiple class namespace for testings for multiples
+
+namespace BerthiaumeKyle_Exercise1
+{
+    // Description: This test program prints out numbers 1-100. If the numbers are
+    // multiples of 2, 7, or both the output to the console will be changed to
+    // "Cross", "Shield", and "CrossShield" respectively. Checking for mulptiles
+    // is accomplished using methods from the Multiple class.
+    class TestProgram
+    {
+        static void Main(string[] args)
+        {
+            // Create Multiple object to test for multiples
+            Multiple integer = new Multiple();
+
+            // Print out numbers 1-100 if the number is a 
+            // multiple of 2 print "Cross." If the number is
+            // a multiple of 7 print "Shield."
+            for(int i=1; i<101; i++)
+            {
+                // set integer property Number to i
+                integer.Number = i;
+                // if integer is a multiple of 2 and 7 print "CrossShield"
+                if (integer.isMultipleOfTwo() && integer.isMultipleOfSeven())
+                    Console.WriteLine("CrossShield");
+                // if integer is a multiple of 7 print "Shield"
+                else if (integer.isMultipleOfSeven())
+                    Console.WriteLine("Shield");
+                // if integer is a multiple of 2 print "Cross"
+                else if (integer.isMultipleOfTwo())
+                    Console.WriteLine("Cross");
+
+                // if integer is neither a multiple of 2 or 7 print integer
+                else
+                    Console.WriteLine(integer.ToString());
+            }
+        }
+    }
+}
---
+++ BerthiaumeKyle_Exercise1/Multiple/Multiple.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berthiaume_Multiple
+{
+    // Description: Class Multiple is simply used to check if a Number is a
+    // multiple of 2 or 7.
+
+    public class Multiple : IMultiple
+    {
+        // Properties------------------------------------------
+        public int Number { get; set; } // Represents int to be tested
+
+        //Default Constructor
+        public Multiple()
+        {
+            // Assign tempNum to Property: Number
+            Number = 0;
+        }
+
+        // Constructor
+        public Multiple(int tempNum)
+        {
+            // Assign tempNum to Property: Number
+            Number = tempNum;
+        }
+
+        // Methods---------------------------------------------
+
+        // isMultipleOfSeven Method
+        // Description: returns true or false based on whether
+        // or not Number is a multiple of 7
+        public bool isMultipleOfSeven()
+        {
+            if (Number % 7 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        // isMultipleOfSeven Method
+        // Description: returns true or false based on whether
+        // or not Number is a multiple of 2
+        public bool isMultipleOfTwo()
+        {
+            if (Number % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        // ToString
+        public override string ToString()
+        {
+            return Number.ToString();
+        }
+    }
+}
